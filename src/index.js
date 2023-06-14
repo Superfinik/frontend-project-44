57d1cddf8083a441bc38c64fda23a529cde461cb
Superfinik <konst.malgin@gmail.com>
2023-06-14 14:56:06 +0300

    adding brain-gcd, updating Makefile, package and index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ function getRandomNumber(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 }
 
+function NOD(x, y) {
+  if (y > x) return NOD(y, x);
+  if (!y) return x;
+  return NOD(y, x % y);
+}
+
 export {
-  greeting, getRandomNumber,
+  greeting, getRandomNumber, NOD,
 };
