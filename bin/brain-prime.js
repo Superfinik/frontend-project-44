b3fb05c56d1a5cd30727b9c101ba358791e84c60
Superfinik <konst.malgin@gmail.com>
2023-06-16 14:58:44 +0300

    adding brain-prime, updating Makefile and package.json

---
+++ bin/brain-prime.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!\nAnswer "yes" if the number is prime, otherwise answer "no".'`);
+
+const brainPrime = () => {
+  function getRandomNumber(min, max) {
+    return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+  }
+
+  let i = 1;
+  while (i <= 3) {
+    const randomNumber = getRandomNumber(1, 100);
+    console.log(`Question: ${randomNumber}`);
+
+    const isPrime = (num) => {
+      for (let n = 2; n < num; n += 1) {
+        if (num % n === 0) return false;
+      }
+      return num !== 1;
+    };
+
+    const trueAnswer = isPrime(randomNumber) ? 'yes' : 'no';
+
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === trueAnswer) {
+      i += 1;
+      console.log('Correct!');
+    } else if (userAnswer !== trueAnswer) {
+      i -= 1;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.\nLet's try again, ${userName}!`);
+      break;
+    }
+  }
+  if (i >= 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+brainPrime();
