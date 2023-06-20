d3b32970aa0b9eeaf798f590b55a0ec12a3593be
Superfinik <konst.malgin@gmail.com>
2023-06-20 15:54:17 +0300

    updating again README and some correction to brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,7 +4,7 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 
 const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".'`);
+console.log(`Hello, ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
 
 const brainEven = () => {
   function getRandomNumber(min, max) {
